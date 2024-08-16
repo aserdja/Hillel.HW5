6ff085fadc7c7b575e88b379c4cead2e019d7118
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-08-16 19:03:13 +0300

    Created class for lambda expressions test

---
+++ HW5/HW5/NumberFilter.cs
@@ -0,0 +1,24 @@
+﻿namespace HW5
+{
+	public static class NumberFilter
+	{
+		public static IEnumerable<int> ReturnWhere(this IEnumerable<int> list, Func<int, bool> lambda)
+		{
+			foreach (var item in list)
+			{
+				if (lambda(item))
+				{
+					yield return item;
+				}
+			}
+		}
+
+		public static void ShowList(IEnumerable<int> list)
+		{
+			foreach (var item in list)
+			{
+				Console.Write($"{item}, ");
+			}
+		}
+	}
+}
